de900c860cdaf2bc74a00d444c8b63eb16f5d705
l0h01up <hamiltonjlewis@gmail.com>
2019-08-06 08:45:53 -0500

    NameForm

---
+++ src/components/NameForm.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+
+class NameForm extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+    }
+  
+    handleChange = (event) =>{
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit = (event) =>{
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <div>
+            <h1>{this.state.value}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </div>
+      );
+    }
+  }
+
+export default NameForm;
